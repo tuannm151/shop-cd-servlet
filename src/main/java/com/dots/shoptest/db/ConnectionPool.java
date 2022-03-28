7db2ba07b4feb69470f179e31fe9ba22797abe9d
Minh Tuan <tuanxsokoh@gmail.com>
2022-03-29 06:12:03 +0700

    fixed datasource

--- src/main/java/com/dots/shoptest/db/ConnectionPool.java
+++ src/main/java/com/dots/shoptest/db/ConnectionPool.java
@@ -1,24 +1,29 @@
 package com.dots.shoptest.db;
 
+import org.apache.commons.dbcp.BasicDataSource;
+
 import java.sql.Connection;
 import java.sql.SQLException;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
 
 public class ConnectionPool {
 
-  private static DataSource dataSource;
+  private static final BasicDataSource ds = new BasicDataSource();
 
   static {
-    try {
-      InitialContext ctx = new InitialContext();
-      dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/shoptest");
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    ds.setDriverClassName("org.postgresql.Driver");
+    ds.setUrl("jdbc:postgresql://ec2-54-235-98-1.compute-1.amazonaws.com:5432/dfeseieja52qb8?sslmode=require");
+    ds.setUsername("nrprnwnrzwnulp");
+    ds.setPassword("816876c29ce67b11f5cb61886c0f7eaa681cec992fec728f1da6aa0bf2d6add4");
+    ds.setMinIdle(3);
+    ds.setMaxIdle(8);
+    ds.setMaxWait(60000);
+    ds.setMaxActive(20);
+    ds.setMaxOpenPreparedStatements(100);
   }
 
   public static Connection getConnection() throws SQLException {
-    return dataSource.getConnection();
+    return ds.getConnection();
   }
+
+  private void DBCPDataSource(){ }
 }
